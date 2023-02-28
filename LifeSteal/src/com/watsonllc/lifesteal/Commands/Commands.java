f4c41946b8694a6a51de54025062f54e61bce811
DaytonJWatson <daytonwatson565@gmail.com>
2023-02-27 19:03:00 -0700

    added commands

---
+++ LifeSteal/src/com/watsonllc/lifesteal/Commands/Commands.java
@@ -0,0 +1,7 @@
+package com.watsonllc.lifesteal.Commands;
+
+public class Commands {
+	public static void setup() {
+		
+	}
+}
--- LifeSteal/src/com/watsonllc/lifesteal/Lifesteal.java
+++ LifeSteal/src/com/watsonllc/lifesteal/Lifesteal.java
@@ -2,12 +2,18 @@ package com.watsonllc.lifesteal;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.watsonllc.lifesteal.Commands.Commands;
+import com.watsonllc.lifesteal.Config.Config;
+
 public class Lifesteal extends JavaPlugin {
 	public static Lifesteal instance;
 	
 	@Override
 	public void onEnable() {
 		instance = this;
+		
+		Commands.setup();
+		Config.setup();
 	}
 	
 }
